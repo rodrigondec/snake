32d130249a042440c96b8df7329bf6a4d4189c48
RodrigoNdeC <rodrigondec@gmail.com>
2016-05-28 23:27:41 -0300

    Classe snake pronta

--- snake.cpp
+++ snake.cpp
@@ -1,8 +1,17 @@
 snake::snake(int i, int j){
-	simbolo = '0';
-	tamanho = 1;
-	posicao.seti(i);
-	posicao.setj(j);
+	direcao = 1;
+	simbolo_cabeca = '0';
+	simbolo_corpo = 'x';
+	tamanho = 3;
+
+	cabeca.seti(i);
+	cabeca.setj(j);
+
+	position parte2(i, (j-1));
+	corpo.push_back(parte2);
+
+	position parte3(i, (j-2));
+	corpo.push_back(parte3);
 }
 
 snake::~snake(){}
@@ -11,14 +20,98 @@ void snake::comer(){
 	tamanho++;
 }
 
+void snake::andar(){
+
+	int i = cabeca.geti();
+	int j = cabeca.getj();
+
+	vector<position>::iterator it = corpo.begin();
+
+	switch(direcao){
+    	case 1:
+        	j++;
+
+        	corpo.insert(it, cabeca);
+
+        	cabeca.setj(j);
+
+        	break;
+    	case 2:
+        	i++;
+
+        	corpo.insert(it, cabeca);
+
+        	cabeca.seti(i);
+
+        	break;
+    	case 3:
+        	j--;
+
+        	corpo.insert(it, cabeca);
+
+        	cabeca.setj(j);
+
+        	break;
+    	case 4:
+        	i--;
+
+        	corpo.insert(it, cabeca);
+
+        	cabeca.seti(i);
+
+        	break;
+	}
+}
+
+bool snake::colisao_corpo(){
+	for(int i = 0; i < corpo.size(); ++i){
+		return cabeca.equals(corpo[i]);
+	}
+}
+
+bool snake::colisao(position x){
+	return cabeca.equals(x);
+}
+
+bool snake::colisao(int i, int j){
+	return cabeca.equals(i, j);
+}
+
+void snake::cortar(){
+	corpo.pop_back();
+}
+
 int snake::gettamanho(){
 	return tamanho;
 }
 
-char snake::getsimbolo(){
-	return simbolo;
+char snake::getsimbolo_cabeca(){
+	return simbolo_cabeca;
 }
 
-position snake::getposicao(){
-	return posicao;
+char snake::getsimbolo_corpo(){
+	return simbolo_corpo;
+}
+
+position snake::getcabeca(){
+	return cabeca;
+}
+
+position snake::getcorpo(int index){
+	return corpo[index];
+}
+
+void snake::setdirecao(int d){
+	direcao = d;
+}
+
+void snake::print(){
+	std::cout<<"Cabeça: ";
+	cabeca.print();
+	std::cout<<std::endl;
+	for (int i = 0; i < corpo.size(); ++i){
+		std::cout<<"Corpo "<<i<<": ";
+		corpo[i].print();
+		std::cout<<std::endl;
+	}
 }
--- snake.h
+++ snake.h
@@ -1,6 +1,7 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
+#include <vector>
 #include "position.h"
 
 using namespace std;
@@ -8,21 +9,37 @@ using namespace std;
 class snake{
 
 private:
+	int direcao;
 	int tamanho;
-	char simbolo;
-	position posicao;
+	char simbolo_cabeca;
+	char simbolo_corpo;
 
+	position cabeca;
 	vector <position> corpo;
 
 public:
 	snake(int i, int j);
 	~snake();
+
 	void comer();
+	void andar();
+
+	bool colisao_corpo();
+	bool colisao(position x);
+	bool colisao(int i, int j);
+
+	void cortar();
 
 	int gettamanho();
-	char getsimbolo();
+	char getsimbolo_cabeca();
+	char getsimbolo_corpo();
 
-	position getposicao();
+	void setdirecao(int d);
+
+	position getcabeca();
+	position getcorpo(int index);
+
+	void print();
 };
 
 #endif
